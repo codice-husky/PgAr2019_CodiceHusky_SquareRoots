155c8bbdbc736d0f20713a4ac54a96d8834b1133
Mattia Orioli <m.orioli002@studenti.unibs.it>
2019-05-14 21:47:21 +0200

    Risolto parte del problema, fortuna che ci penso io

--- src/squareRoots/Node.java
+++ src/squareRoots/Node.java
@@ -1,40 +1,42 @@
 package squareRoots;
-
-
-public class Node {	
-	
-	public static final int VALORE_MAX = 11;
-	
-	static String valore;
-	static Node sx;
-	static Node dx;
-	
-	
-	public Node() {
-		int val = (int) Math.floor(Math.random()*2);
-		System.out.println(val);
-		if(val == 0) {	//segno
-			switch((int)Math.random()*4+1) {
-				case 1: this.valore = "+"; break;
-				case 2: this.valore = "-"; break;
-				case 3: this.valore = "*"; break;
-				case 4: this.valore = "/"; break;
-			}
-			this.sx= new Node();
-			this.dx= new Node();
-		}else { //numero
-			this.valore = ""+Math.random()*VALORE_MAX;
-		}
-	}
-	
-	public String getTheEspressione(){
-		try {
-			int x = Integer.parseInt(valore);
-			return ""+x;
-		}catch(NumberFormatException e) {
-			return ( sx.getTheEspressione()+valore+dx.getTheEspressione());
-		}
-	}
-	
-	
+ 
+ 
+public class Node {
+   
+    public static final int VALORE_MAX = 11;
+   
+    String valore;
+    Node sx;
+    Node dx;
+   
+   
+    public Node() {
+        int val = (int) Math.floor(Math.random()*2);
+        System.out.println(val);
+        if(val == 0) {  //segno
+            int prova = (int)Math.random()*4+1;
+            switch(prova) {
+                case 1: this.valore = "+"; break;
+                case 2: this.valore = "-"; break;
+                case 3: this.valore = "*"; break;
+                case 4: this.valore = "/"; break;
+            }
+            this.sx= new Node();
+            this.dx= new Node();
+        }else { //numero
+            this.valore =  ""+(int)((Math.random()*VALORE_MAX)+1);
+        }
+    }
+   
+    public String getTheEspressione(){
+        try {
+            int x = Integer.parseInt(valore);
+            return ""+x;
+        }catch(NumberFormatException e) {
+            String risp = "( "+sx.getTheEspressione()+valore+dx.getTheEspressione()+" )";
+            return (risp);
+        }
+    }
+   
+   
 }
